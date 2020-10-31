67a0748421a95e548ca39653940af8c6981396e5
Mickocar <milovan.maric@gmail.com>
2020-10-31 12:24:08 -0400

    Tird commit

--- src/test/java/HelloWeenTest.java
+++ src/test/java/HelloWeenTest.java
@@ -3,5 +3,7 @@ public class HelloWeenTest {
     public static void main(String[] args) {
         System.out.println("Hello world");
         // I am the best and the smartest
+        //Let's play
+        //I want to be the best
     }
 }
